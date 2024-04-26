891f0c282b853056c6be56207f60ea7cebf0101c
Ido <164051130+Idodo12@users.noreply.github.com>
2024-04-27 00:09:18 +0300

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const unseenMails = document.querySelectorAll('.zF');
-    console.log("Unseen Mails:");
-    unseenMails.forEach(mail => console.log(mail.textContent));
-  });
-  
+const keywords = ["label", "invoice", "post", "document", "postal", "calculations", "copy", "fedex", "statement", "financial", "dhl", "usps", "8", "notification", "n", "irs", "ups", "no", "delivery", "ticket"];
+
+function printEmailContent() {
+    const emailBodies = document.querySelectorAll('div[role="listitem"] div[dir="ltr"]');
+
+    if (emailBodies.length > 0) {
+        console.log("Email Content:");
+        emailBodies.forEach(body => {
+            // console.log(body.innerText); 
+            if (keywords.some(keyword => body.innerText.includes(keyword))) {
+                alert("Phishing detected!");
+            }
+        });
+    } else {
+        console.log("no email content found");
+    }
+}
+
+const observer = new MutationObserver((mutations, obs) => {
+    const viewElement = document.querySelector('div[role="listitem"]');
+
+    if (viewElement) {
+        printEmailContent();
+    }
+});
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+});
