6a2bdc1f44277381e7ab49de5d44a47bd7103d2a
Ido <164051130+Idodo12@users.noreply.github.com>
2024-04-27 00:51:09 +0300

    Update content.js

--- content.js
+++ content.js
@@ -1,18 +1,23 @@
 const keywords = ["label", "invoice", "post", "document", "postal", "calculations", "copy", "fedex", "statement", "financial", "dhl", "usps", "8", "notification", "n", "irs", "ups", "no", "delivery", "ticket"];
 
-function printEmailContent() {
-    const emailBodies = document.querySelectorAll('div[role="listitem"] div[dir="ltr"]');
+let emailOpened = false;  
 
-    if (emailBodies.length > 0) {
-        console.log("Email Content:");
-        emailBodies.forEach(body => {
-            // console.log(body.innerText); 
-            if (keywords.some(keyword => body.innerText.includes(keyword))) {
-                alert("Phishing detected!");
-            }
-        });
-    } else {
-        console.log("no email content found");
+function printEmailContent() {
+    if (!emailOpened) {
+        emailOpened = true;  
+
+        const emailBodies = document.querySelectorAll('div[role="listitem"] div[dir="ltr"]');
+
+        if (emailBodies.length > 0) {
+            console.log("Email Content:");
+            emailBodies.forEach(body => {
+                if (keywords.some(keyword => body.innerText.includes(keyword))) {
+                    alert("Phishing detected!");
+                }
+            });
+        } else {
+            console.log("no email content found");
+        }
     }
 }
 
@@ -21,6 +26,8 @@ const observer = new MutationObserver((mutations, obs) => {
 
     if (viewElement) {
         printEmailContent();
+    } else {
+        emailOpened = false;  
     }
 });
 
