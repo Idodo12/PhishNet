a868ea3738ea3ff30df0da0680847a857f277034
Ido <164051130+Idodo12@users.noreply.github.com>
2024-04-27 00:09:46 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,9 +1,4 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && tab.url.startsWith('https://mail.google.com')) {
-      chrome.scripting.executeScript({
-        target: { tabId: tabId },
-        files: ['content.js']
-      });
-    }
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Extension installed");
   });
   
