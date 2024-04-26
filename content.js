c717b4c7b24272b9b09ab05da2609a995e24d6cf
Ido <164051130+Idodo12@users.noreply.github.com>
2024-04-27 01:43:13 +0300

    Update content.js

--- content.js
+++ content.js
@@ -1,31 +1,30 @@
-const { pipeline } = require("@huggingface/transformers");
+var keyWords = [
+    "urgent", "calls", "paid", "need", "gift", "gifts", "cards", "card",
+    "urgently", "response", "needed", "login", "expiring", "soon",
+    "immediately", "free", "detect", "pay", "job", "access", "expire", "friend", "lowest",
+    "price", "serious", "action", "database", "winner", "refund", "files", "activate",
+    "activated", "wage", "vital", "irregular", "docs", "invited", "account", "employment",
+    "notice", "service", "bcourse", "employee", "phone", "information", "dirks", "suspended"
+  ];
 
-let emailOpened = false;
-
-async function isPhishing(content) {
-    try {
-        const model = "textattack/albert-base-v2-phishing-trec"; 
-        const classifier = pipeline('zero-shot-classification', { model });
-        const result = await classifier(content, ["phishing", "legitimate"]);
-        return result[0].labels.includes("phishing");
-    } catch (error) {
-        console.error('Error:', error);
-        return false;
-    }
-}
+let emailOpened = false;  
 
 function printEmailContent() {
     if (!emailOpened) {
-        emailOpened = true
-        
+        emailOpened = true;  
+
         const emailBodies = document.querySelectorAll('div[role="listitem"] div[dir="ltr"]');
 
         if (emailBodies.length > 0) {
             console.log("Email Content:");
-            emailBodies.forEach(async body => {
-                const content = body.innerText;
-                const isPhishingContent = await isPhishing(content);
-                if (isPhishingContent) {
+            emailBodies.forEach(body => {
+                console.log(body.innerText);
+                let keywordCount = keyWords.reduce((count, keyword) => {
+                    let regex = new RegExp(`\\b${keyword}\\b`, 'gi');
+                    return count + (body.innerText.match(regex) ? body.innerText.match(regex).length : 0);
+                }, 0);
+
+                if (keywordCount >= 2) {
                     alert("Phishing detected!");
                 }
             });
